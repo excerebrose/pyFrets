18fc0539ad1d6e13994ed48142aeeed4856a2b75
Harjyot Singh <harjyotsingh@Harjyots-MacBook-Pro.local>
2023-12-22 15:59:39 +0300

    Upgrade to py3

--- guitarfretboard.py
+++ guitarfretboard.py
@@ -20,10 +20,10 @@ def generateScale(rootNote,scaleIntervals):
 def main():
 	scaleSteps = {'maj':[2,2,1,2,2,2,1],'natMin':[2,1,2,2,1,2,2],'pentMin':[3,2,2,3,2],'blues':[3,2,1,1,3,2],'pentMaj':[2,2,3,2,3],'harMin':[2,1,2,2,1,3,1],'melMin':[2,1,2,2,2,2,1]}
 	parser = argparse.ArgumentParser(description='Prints fretboard for tunning given')
-	parser.add_argument('-t','--tuning',nargs=6, choices = notes+ map(lambda x:x.lower(),notes),help=argparse.SUPPRESS)
-	parser.add_argument('-n','--frets', type=int,choices=range(1,25),help=argparse.SUPPRESS)
+	parser.add_argument('-t','--tuning',nargs=6, choices = notes+ [x.lower() for x in notes],help=argparse.SUPPRESS)
+	parser.add_argument('-n','--frets', type=int,choices=list(range(1,25)),help=argparse.SUPPRESS)
 	group = parser.add_mutually_exclusive_group()
-	group.add_argument('-f','--find',type=str,choices=notes+ map(lambda x:x.lower(),notes),help=argparse.SUPPRESS)
+	group.add_argument('-f','--find',type=str,choices=notes+ [x.lower() for x in notes],help=argparse.SUPPRESS)
 	group.add_argument('-s','--scale',nargs=2,action='append',help=argparse.SUPPRESS)
 	args = parser.parse_args()
 	tuning = args.tuning
@@ -32,16 +32,16 @@ def main():
 	scale = args.scale
 	if tuning is None:
 		tuning = ['E','A','D','G','B','E']
-	tuning = map(lambda x:x.upper(),tuning)
+	tuning = [x.upper() for x in tuning]
 	if numberOfFrets is None:
 		numberOfFrets = 12	
 
 	if scale is not None:
-			if scale[0] in map(lambda x:list(x),(product(notes,(scaleSteps.keys())))):
+			if scale[0] in [list(x) for x in (product(notes,(list(scaleSteps.keys()))))]:
 				scaleRoot = scale[0][0]
 				scaleType = scaleSteps[scale[0][1]]
 				scaleNotes = generateScale(scaleRoot,scaleType)
-				print "Scale Notes are: " + '-'.join(scaleNotes)
+				print("Scale Notes are: " + '-'.join(scaleNotes))
 				if scale[0][1] == "pentMaj":
 					relativeScale = generateScale(scaleRoot,scaleSteps['maj'])
 					chords = getChords(relativeScale)
@@ -51,27 +51,27 @@ def main():
 				else:
 					chords = getChords(scaleNotes)
 				chordChart = PrettyTable(["Chord Type", "Root", "Third", "Fifth"])
-				print "Chords in scale are:"
+				print("Chords in scale are:")
 				for v in chords:
 					chordChart.add_row([v[0][0] + " " + v[1],v[0][0],v[0][1],v[0][2]])
-				print chordChart
+				print(chordChart)
 			else:
 				raise ValueError("Incorrect scale input, see help!")
 				exit()
 	
 	fretboard = PrettyTable()
-	fretboard.add_column("0",range(1,numberOfFrets+1))
+	fretboard.add_column("0",list(range(1,numberOfFrets+1)))
 	for i in range(len(tuning)):
 		current_note = tuning[i]
 		notesIterator = dropwhile(lambda x:x!=current_note,cycled_notes)
 		noteList = (list(islice(notesIterator,1,numberOfFrets+1)))
 		if elementToShow is not None:
-			noteList = map(lambda x: x if x==elementToShow else 'x',noteList)
+			noteList = [x if x==elementToShow else 'x' for x in noteList]
 		elif scale is not None:
-			noteList = map(lambda x: x if x in scaleNotes else 'x',noteList)
+			noteList = [x if x in scaleNotes else 'x' for x in noteList]
 		fretboard.add_column(current_note,noteList)
-	print "\nFretboard--->"
-	print fretboard
+	print("\nFretboard--->")
+	print(fretboard)
 
 def getChords(scale_notes):
 	len_scale = len(scale_notes)
